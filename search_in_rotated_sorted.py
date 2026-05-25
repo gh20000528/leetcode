4ba6b7e5e18ffeb5c42adca43dd451b62731feca
han sheng <gh890528@gmail.com>
2026-05-25 14:11:07 +0800

    加註解

--- search_in_rotated_sorted.py
+++ search_in_rotated_sorted.py
@@ -20,15 +20,21 @@ def search(self, nums: List[int], target: int) -> int:
         mid = (left + right) // 2
         if nums[mid] == target:
             return mid
-        
+
+        # 左邊 sorted
         if nums[mid] <= nums[mid]:
+            # target 在左半邊
             if nums[left] <= target < nums[mid]:
                 right = mid - 1
+            # target 不在左半邊    
             else:
                 left = mid + 1
+        # 右邊 sorted
         else:
+            # target 在右半邊
             if nums[mid] < target <= nums[right]:
                 left = mid + 1
+            # target 不在右半邊
             else:
                 right = mid - 1
     return -1
