ca151e476f0924b3326855bf87dc9add01c42b25
han sheng <gh890528@gmail.com>
2026-06-26 14:04:48 +0800

    kth largest element in a stream

---
+++ kth_largest.py
@@ -0,0 +1,29 @@
+# Example 1:
+
+# Input:
+# ["KthLargest", "add", "add", "add", "add", "add"]
+# [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+
+# Output: [null, 4, 5, 5, 8, 8]
+
+# Explanation:
+
+# KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+# kthLargest.add(3); // return 4
+# kthLargest.add(5); // return 5
+# kthLargest.add(10); // return 5
+# kthLargest.add(9); // return 8
+# kthLargest.add(4); // return 8
+
+
+
+class KthLargest: # O((M+N)⋅logk)  O(k)
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.nums = nums
+
+
+    def add(self, val: int) -> int:
+        self.nums.append(val)
+        self.nums.sort(reversed=True)
+        return self.nums[self.k - 1]
