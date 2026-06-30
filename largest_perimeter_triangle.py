e52f97c3db43fe4b06e25fc377dc1370f9e824fa
han sheng <gh890528@gmail.com>
2026-06-30 16:33:13 +0800

    larget perimeter triangle

---
+++ largest_perimeter_triangle.py
@@ -0,0 +1,17 @@
+# Example 1:
+
+# Input: nums = [2,1,2]
+# Output: 5
+# Explanation: You can form a triangle with three side lengths: 1, 2, and 2.
+
+
+def largestPerimeter(self, nums: List[int]) -> int:
+    nums.sort()
+    for i in range(len(nums) - 1, 1, -1):
+        a = nums[i - 2]
+        b = nums[i - 1]
+        c = nums[i]
+
+        if a + b > c:
+            return a + b + c
+    return 0
